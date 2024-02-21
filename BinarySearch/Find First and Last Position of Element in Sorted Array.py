e135d73af48855469974c686037e4fd4f4ee9dd0
Nilesh-NS-11 <136571158+Nilesh-NS-11@users.noreply.github.com>
2024-02-21 18:30:14 +0530

    Add files via upload

---
+++ BinarySearch/Find First and Last Position of Element in Sorted Array.py
@@ -0,0 +1,45 @@
+"""
+
+Find First and Last Position of Element in Sorted Array
+
+ an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+
+"""
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = self.binSearch(nums, target, True)
+        right = self.binSearch(nums, target, False)
+        return [left,right]
+
+    def binSearch(self, nums, target, leftBias):
+        l, r = 0, len(nums) - 1
+        i = -1
+        while l <= r:
+            m = l + (r - l) // 2
+            if nums[m] < target:
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                i = m 
+                if leftBias:
+                    r = m - 1
+                else:
+                    l = m + 1
+        return i
