0f59578f68087d0fe60eb88575f4f2d7df043dac
Nilesh-NS-11 <136571158+Nilesh-NS-11@users.noreply.github.com>
2024-03-07 05:48:45 -0800

    Add files via upload

---
+++ Trees/Path Sum.py
@@ -0,0 +1,54 @@
+"""
+
+Path Sum
+
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+Example 3:
+
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        rem = [(root, targetSum - root.val)]
+        while rem:
+            node, cur_sum = rem.pop()
+            if not node.left and not node.right and cur_sum == 0:
+                return True
+            if node.right:
+                rem.append((node.right, cur_sum - node.right.val))
+            if node.left:
+                rem.append((node.left, cur_sum - node.left.val))
+        return False
+        
